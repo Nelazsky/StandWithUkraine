dbe9747c60bd41736bb945ec5f21afdea2cecb82
Vladimir Nelazsky <nelazsky.vladimir@gmail.com>
2022-05-09 16:21:04 +0300

    Change image && add text replacing

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 let images = [
-    "https://picjumbo.com/wp-content/uploads/i-stand-with-ukraine-profile-picture-free-photo-2210x2210.jpg",
+    "https://www.evermore.ca/hubfs/Stand%20With%20Ukraine%20Digital.jpeg",
     "https://standwithukraine.live/wp-content/uploads/2022/02/cropped-IMG_1928.png",
     "https://www.president.gov.ua/storage/j-image-storage/14/89/73/5a855047042111f63fd222a15953569f_1579673033_extra_large.jpeg",
 ];
@@ -8,4 +8,14 @@ const pageImages = document.getElementsByTagName("img");
 for (let i = 0; i < pageImages.length; i++) {
     const randomImg = Math.floor(Math.random() * images.length);
     pageImages[i].src = images[randomImg];
+}
+
+const pageHeaders = document.getElementsByTagName("h2");
+for (let i = 0; i < pageHeaders.length; i++) {
+    pageHeaders[i].innerText = "Stand With Ukraine";
+}
+
+const pageContents = document.getElementsByTagName("p");
+for (let i = 0; i < pageContents.length; i++) {
+    pageHeaders[i].innerText = "Ще не вмерла України і слава, і воля,\n Ще нам, браття молодії, усміхнеться доля.";
 }
